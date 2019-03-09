bf42a435504f41aa9bea6284464b7308e250e8f2
Roman <romankudyba1999@gmail.com>
2019-03-09 20:25:40 +0200

    Lab1IOS

---
+++ Lab1Ios/Lab1Ios/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Lab1Ios
+//
+//  Created by Роман К on 3/9/19.
+//  Copyright © 2019 Роман К. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Lab1Ios/Lab1Ios/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Lab1Ios
+//
+//  Created by Роман К on 3/9/19.
+//  Copyright © 2019 Роман К. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
